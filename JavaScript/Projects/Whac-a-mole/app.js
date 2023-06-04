c56120f22e053a637280b610964c419c9d05b52c
Deathriad <69326541+Deathriad@users.noreply.github.com>
2023-06-04 12:56:03 +0530

    starting with a new game called Whac-a-mole

---
+++ JavaScript/Projects/Whac-a-mole/app.js
@@ -0,0 +1,4 @@
+const squares=document.querySelectorAll(".square")
+const mole = document.querySelector(".mole")
+const timeLeft=document.querySelector("#time-lleft")
+const score=document.querySelector("#score")
